21af93204a87df8bd3f944c6759739bdbcf0849c
BhavaAnjum <bhava1410@gmail.com>
2024-05-28 12:06:25 +0530

    update logger and github action ci

--- test/features/demo/demo.feature
+++ test/features/demo/demo.feature
@@ -5,7 +5,7 @@ Given Google page is opened
 When Search with <SearchItem>
 Then Click on the search result
 Then Url should match <ExpectedURL>
-
+Then Go to new url
 Examples:
     | TestID     |SearchItem |ExpectedURL               |
     | DEMO_TC001 |WDIO       |https://webdriver.io/     |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,25 +1,15 @@
-// import { Given } from "@wdio/cucumber-framework";
-// import * as chai from 'chai'
-// Given(/^Google page is opened$/, async function(){
-//    await browser.url('https://www.google.com');
-//     await browser.pause(4000);
-//     let URL=await browser.getUrl()
-//     chai.expect(URL).to.equal('https://www.google.com')  
-// });
-// When(/^Search with <SearchItem>$/, async function(SearchItem){
-//      let search = $('#input');
-//     await search.setValue(SearchItem);
-
-// })
 import { Given, When, Then } from "@cucumber/cucumber";
 import * as chai from 'chai'
+import logger from "../helper/logger.ts";
 
 Given(/^Google page is opened$/, async function () {
+    logger.info(`Starting`)
     await browser.url("https://www.google.com")
     await browser.pause(1000)
 })
 
 When(/^Search with (.*)$/, async function (searchItem) {
+    logger.info(`searching for: ${this.SearchItem}`)
     let ele = await $(`[name=q]`)
     await ele.setValue(searchItem)
     await browser.keys("Enter")
@@ -36,5 +26,29 @@ Then(/Url should match (.*)$/, async function (ExpectedURL) {
     }, {timeout: 20000, interval: 500, timeoutMsg: `Failed loading WDIO web page: ${await browser.getTitle()}`})
 
     let url = await browser.getUrl()
+    console.log(url)
     chai.expect(url).to.equal(ExpectedURL)
+            await $(".featureTitle_r_DH").scrollIntoView()
+            await browser.pause(3000)
 })
+            // await $("#mousehover").moveTo()
+            // await browser.pause(3000)
+            // await $("=Top").click()
+Then(/^Go to new url$/, async function () {
+
+    await browser.url("http://only-testing-blog.blogspot.com/2014/09/selectable.html")
+    await $(".scroll").scrollIntoView()
+    await browser.pause(3000)
+    // await $("#mousehover").moveTo()
+    // await browser.pause(3000)
+    // await $("=Top").click()
+    // await $("button").doubleClick()
+    // browser.pause(6000)
+    await browser.saveScreenshot("screenshot.png")
+    //chai.expect(await browser.isAlertOpen()).to.be.true
+    // chai.expect(await browser.getAlertText()).to.equal("You double clicked me.. Thank You..")
+    // await browser.acceptAlert()
+    // await browser.pause(3000)
+})
+        
+
